f9a88a15fddbc00057a3a944d7a0ac01b0bbc104
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-11-23 12:42:16 +0700

    melakukan validasii data

---
+++ configs/verify.js
@@ -0,0 +1,22 @@
+const verifyInput = (req, res, next) => {
+    const { name, category, type, quantity, extra_info } = req.body;
+
+    // Validasi data
+    if (!name || !category || !type || !quantity) {
+        return res.status(400).json({ error: 'Data tidak lengkap. Pastikan semua kolom diisi!' });
+    }
+
+    // Validasi tipe data
+    if (typeof name !== 'string' || typeof category !== 'string' || typeof type !== 'string') {
+        return res.status(400).json({ error: 'Kolom name, category, dan type harus berupa teks.' });
+    }
+
+    if (typeof quantity !== 'number') {
+        return res.status(400).json({ error: 'Kolom quantity harus berupa angka.' });
+    }
+
+    // Jika validasi berhasil, lanjut ke middleware berikutnya
+    next();
+};
+
+module.exports = { verifyInput };
